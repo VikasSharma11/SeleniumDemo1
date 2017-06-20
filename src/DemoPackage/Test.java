1360d49a3ad7509fc84ed0ca36ef6caca38bf8ab
Vikas Sharma <vsharma@salmon.com>
2017-06-20 15:48:27 +0530

    test

--- src/DemoPackage/Test.java
+++ src/DemoPackage/Test.java
@@ -1,11 +1,9 @@
 package DemoPackage;
 
-import net.sourceforge.htmlunit.corejs.javascript.tools.debugger.Main;
-
 public class Test {
-public static void main(String[] args) {
-//	String s = "registrationError.zipCode";
-//	System.out.println(s.split("\\.")[1]);
-	System.out.println("In local branch Vikas");
-}
+	public static void main(String[] args) {
+		// String s = "registrationError.zipCode";
+		// System.out.println(s.split("\\.")[1]);
+		System.out.println("In local branch Vikas");
+	}
 }
