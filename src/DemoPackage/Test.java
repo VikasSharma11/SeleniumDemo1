05bcf4411bfd9092e8cbceafb60cfd7691864008
Vikas Sharma <vsharma@salmon.com>
2017-06-21 13:44:52 +0530

    test

--- src/DemoPackage/Test.java
+++ src/DemoPackage/Test.java
@@ -2,9 +2,8 @@ package DemoPackage;
 
 public class Test {
 	public static void main(String[] args) {
-		// String s = "registrationError.zipCode";
-		// System.out.println(s.split("\\.")[1]);
+		 String s = "registrationError.zipCode";
+		 System.out.println(s.split("\\.")[1]);
 		System.out.println("In local branch Vikas");
-		System.out.println("test ");
 	}
 }
